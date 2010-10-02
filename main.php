cbcb8f5cf40370d1a7d95e888ae1599e48be702b
Mitchell Hislop <mitch@mitchellhislop.com>
2010-10-02 01:59:11 -0500

    plugin structure

--- main.php
+++ main.php
@@ -5,3 +5,27 @@ Version: 0.1a
 Author: Mitchell Hislop
 Description: Displays Get Glue information on your blog as a widget
 */
+if (!class_exists("GetGlue")) 
+{
+    class GetGlue {
+    	function GetGlueConst() { //constructor
+           
+        						
+        						}
+   
+    			}
+ 
+}
+
+if (class_exists("GetGlue")) {
+    $glue = new GetGlue();
+}
+
+if (isset($glue))
+	{
+	
+		//actions
+		
+		//filters
+		
+	}
