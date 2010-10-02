225b53fd14e59f176231ff6c887584cdb7c4ce68
Mitchell Hislop <mitch@mitchellhislop.com>
2010-10-02 01:55:05 -0500

    plugin manifest

---
+++ main.php
@@ -0,0 +1,7 @@
+/*
+Plugin Name: Get Glue WP Plugin
+Plugin URI: http://mitchellhislop.com
+Version: 0.1a
+Author: Mitchell Hislop
+Description: Displays Get Glue information on your blog as a widget
+*/
