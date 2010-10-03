9922fc47c9d47505b7ff12559ec05e3a6d2b91f8
Mitchell Hislop <mitch@mitchellhislop.com>
2010-10-03 01:58:04 -0500

    starting oauth work

--- main.php
+++ main.php
@@ -5,6 +5,7 @@ Version: 0.1a
 Author: Mitchell Hislop
 Description: Displays Get Glue information on your blog as a widget
 */
+include ('php/ggwidget.php');
 if (!class_exists("GetGlue")) 
 {
     class GetGlue {
@@ -17,7 +18,8 @@ if (!class_exists("GetGlue"))
  
 }
 
-if (class_exists("GetGlue")) {
+if (class_exists("GetGlue")) 
+{
     $glue = new GetGlue();
 }
 
@@ -25,7 +27,26 @@ if (isset($glue))
 	{
 	
 		//actions
-		
+		add_action('admin_menu', 'get_glue_menu');
+		add_action('widgets_init', create_function('', 'return register_widget("FooWidget");'));
+		function get_glue_menu() {
+
+ 		 add_options_page('Get Glue Options', 'Get Glue', 'manage_options', 'my-unique-identifier', 'get_glue_options');
+
+}
+
+function get_glue_options() {
+
+  if (!current_user_can('manage_options'))  {
+    wp_die( __('You do not have sufficient permissions to access this page.') );
+  }
+
+  echo '<div class="wrap">';
+  echo '<p>Here is where the form would go if I actually had options.</p>';
+  echo '</div>';
+
+}
+
 		//filters
 		
 	}
--- php/ggwidget.php
+++ php/ggwidget.php
@@ -1,18 +1,37 @@
-class My_Widget extends WP_Widget {
-function My_Widget() {
-    // widget actual processes
-                     }
-function form($instance) {
-  // outputs the options form on admin
-             }
+class FooWidget extends WP_Widget {
+    /** constructor */
+    function FooWidget() {
+        parent::WP_Widget(false, $name = 'FooWidget');	
+    }
 
-function update($new_instance, $old_instance) {
- // processes widget options to be saved
-                     }
-    
-function widget($args, $instance) {
-  // outputs the content of the widget
-                              }
-   
-                            }
+    /** @see WP_Widget::widget */
+    function widget($args, $instance) {		
+        extract( $args );
+        $title = apply_filters('widget_title', $instance['title']);
+        ?>
+              <?php echo $before_widget; ?>
+                  <?php if ( $title )
+                        echo $before_title . $title . $after_title; ?>
+                  Hello, World!
+              <?php echo $after_widget; ?>
+        <?php
+    }
+
+    /** @see WP_Widget::update */
+    function update($new_instance, $old_instance) {				
+	$instance = $old_instance;
+	$instance['title'] = strip_tags($new_instance['title']);
+        return $instance;
+    }
+
+    /** @see WP_Widget::form */
+    function form($instance) {				
+        $title = esc_attr($instance['title']);
+        ?>
+            <p><label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:'); ?> <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" /></label></p>
+        <?php 
+    }
+
+} // class FooWidget
+
  register_widget('My_Widget');
